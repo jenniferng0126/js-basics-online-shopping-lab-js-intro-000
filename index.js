5770fcaaecf6e4cb7e9820a1a4a59ea3fd9c099b
Jennifer Ng <ka.man.jennifer.ng@gmail.com>
2020-08-12 18:22:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   // write your code here
-  if (cardNumber === "" || cardNumber <= 0) {
+  if (cardNumber === undefined) {
     return "Sorry, we don't have a credit card on file for you..";
   } else {
     let totalPrice = total();
